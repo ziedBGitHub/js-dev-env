621cc19e25328fc5bfdd358cd816c8c48e0c661e
Zied BELGUITH <ziedbelguith@MAC280.local>
2018-10-15 14:26:37 +0200

    Adding some API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  // Hard coding for simplicity. Pretend this hits real database
+  res.json([
+    {"id": 1,"firstName": "Rafaela","lastName": "Ruegg","email": "rruegg0@unc.edu"},
+    {"id": 2,"firstName": "Palm","lastName": "Shearsby","email": "pshearsby1@hostgator.com"},
+    {"id": 3,"firstName": "Isidora","lastName": "Keemar","email": "ikeemar2@apple.com"},
+    {"id": 4,"firstName": "Gerek","lastName": "Adam","email": "gadam3@g.co"},
+    {"id": 5,"firstName": "Michaeline","lastName": "Duchesne","email": "mduchesne4@businessinsider.com"},
+    {"id": 6,"firstName": "Sharline","lastName": "Speedin","email": "sspeedin5@oracle.com"},
+    {"id": 7,"firstName": "Jedd","lastName": "Pfaffel","email": "jpfaffel6@statcounter.com"},
+    {"id": 8,"firstName": "Darsie","lastName": "Oldroyd","email": "doldroyd7@dot.gov"},
+    {"id": 9,"firstName": "Mitzi","lastName": "Cowing","email": "mcowing8@pbs.org"},
+    {"id": 10,"firstName": "Ruthy","lastName": "Paget","email": "rpaget9@devhub.com"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
---
+++ src/api/userApi.js
@@ -0,0 +1,16 @@
+import 'whatwg-fetch';
+
+export function getUsers() {
+   return get('users');
+}
+
+function get(url) {
+  return fetch(url).then(onSuccess, onError);
+}
+ function onSuccess(response) {
+   return response.json();
+ }
+
+ function onError(err) {
+   console.log(err); // eslint-disable-line no-console
+ }
